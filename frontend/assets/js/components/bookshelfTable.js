f6b981f6d325196c3f0d46261098d36e11d607bb
HaridraNarayan-900 <72052215s@gmail.com>
2026-01-03 01:28:41 +0000

    Finished

--- frontend/assets/js/components/bookshelfTable.js
+++ frontend/assets/js/components/bookshelfTable.js
@@ -28,6 +28,7 @@ export function renderBookshelfTable(bookshelves) {
 
     // Populate the row with dynamic HTML content using a template literal
     row.innerHTML = `
+      <td class="px-3 py-2">${bookshelf.id}</td>
       <td class="px-3 py-2">${bookshelf.location}</td>
       <td class="px-3 py-2">${bookshelf.description}</td>
       <td class="px-3 py-2 flex space-x-2">
